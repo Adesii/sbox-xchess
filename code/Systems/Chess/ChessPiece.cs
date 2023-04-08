45146a5f26395e588c684f323167953554e5c31f
Adesi <Kopietz123@gmail.com>
2023-04-08 13:35:51 +0200

    Make Colored glow around pieces

--- code/Systems/Chess/ChessPiece.cs
+++ code/Systems/Chess/ChessPiece.cs
@@ -12,6 +12,9 @@ public partial class ChessPiece : Entity
 	[BindComponent]
 	public ChessMoveComponent MoveComponent { get; }
 
+	[BindComponent]
+	public Glow Glow { get; }
+
 	public override void Spawn()
 	{
 		base.Spawn();
@@ -21,6 +24,17 @@ public partial class ChessPiece : Entity
 		{
 			Components.Create<ChessMoveComponent>();
 			Log.Warning( $"Piece {this} has no move component, creating one" );
+		}
+		Tick();
+	}
+
+	[Event.Hotload]
+	public void Tick()
+	{
+		foreach ( var child in Children.OfType<ModelEntity>() )
+		{
+			child.Components.GetOrCreate<Glow>().Color = Team == PlayerTeam.White ? Color.White : Color.FromBytes( 0x8B, 0x45, 0x13 );
+			child.Components.GetOrCreate<Glow>().Width = 0.25f;
 		}
 	}
 }
