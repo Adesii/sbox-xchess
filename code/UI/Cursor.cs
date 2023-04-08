99df61fa7376f554e3a647c8a008e16f036b1599
Adesi <Kopietz123@gmail.com>
2023-04-08 13:35:35 +0200

    Use Real Mouse Position. but can't show it since Visible isn't working anymore

--- code/UI/Cursor.cs
+++ code/UI/Cursor.cs
@@ -4,9 +4,11 @@ public partial class Cursor : Panel
 	[Event.Client.BuildInput]
 	private void buildinput()
 	{
-		FakeMousePosition -= RealMouse - Sandbox.Mouse.Position;
-		FakeMousePosition = FakeMousePosition.Clamp( new Vector2( 0, 0 ), new( Screen.Width, Screen.Height ) );
+		//FakeMousePosition -= RealMouse - Sandbox.Mouse.Position;
+		//FakeMousePosition = FakeMousePosition.Clamp( new Vector2( 0, 0 ), new( Screen.Width, Screen.Height ) );
 		RealMouse = Sandbox.Mouse.Position;
+		FakeMousePosition = RealMouse;
+
 
 	}
 }
